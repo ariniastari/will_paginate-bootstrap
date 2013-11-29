5e012797f15bedd74f91146830288cbd4e0dcf89
ariniastari <astariarini@gmail.com>
2013-11-29 21:45:40 +0700

    change class prev to previous

--- lib/bootstrap_pagination/bootstrap_renderer.rb
+++ lib/bootstrap_pagination/bootstrap_renderer.rb
@@ -50,7 +50,7 @@ module BootstrapPagination
 
     def previous_page
       num = @collection.current_page > 1 && @collection.current_page - 1
-      previous_or_next_page(num, @options[:previous_label], "prev")
+      previous_or_next_page(num, @options[:previous_label], "previous")
     end
 
     def next_page
